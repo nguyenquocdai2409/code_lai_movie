b52a4c65620e67500a4c0ef35576c49cced1a6fa
nguyenphanlongvu <vuvt6019@gmail.com>
2023-12-28 19:29:26 +0700

    update file

--- src/Footer/FooterBody.js
+++ src/Footer/FooterBody.js
@@ -10,7 +10,7 @@ export default function FooterBody() {
         paddingBottom: "24px",
       }}
     >
-      <hr />
+      <hr className="" />
       <div
         style={{
           top: 0,
@@ -21,19 +21,22 @@ export default function FooterBody() {
           textAlign: "left",
         }}
       >
-        <h6 className="MuiGrid-root">
+        <h6 className="font-thin text-xs">
           TIX - SẢN PHẨM CỦA CÔNG TY CỔ PHẦN ZION{" "}
         </h6>
-        <h6>
+        <br />
+        <h6 className="font-thin text-xs">
           Đia chỉ: Z06 Đường số 13, Phường Tân Thuận Đông, Quận 7, Tp. Hồ Chí
           Minh, Việt Nam.
         </h6>
-        <h6>
+        <h6 className="font-thin text-xs">
           Giấy chứng nhận đăng ký kinh doanh số: 0101659783, đăng ký thay đổi
           lần thứ 30, ngày 22 tháng 01 năm 2020 do Sở kế hoạch và đầu tư Thành
           phố Hồ Chí Minh cấp.
         </h6>
-        <h6>Số Điện Thoại (Hotline): 1900 545 436</h6>
+        <h6 className="font-thin text text-xs">
+          Số Điện Thoại (Hotline): 1900 545 436
+        </h6>
       </div>
     </div>
   );
