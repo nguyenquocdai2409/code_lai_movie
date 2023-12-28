1189c788823583a67795b179abd8459117df4455
nguyenphanlongvu <vuvt6019@gmail.com>
2023-12-28 20:50:29 +0700

    update file

--- src/Footer/FooterBody.js
+++ src/Footer/FooterBody.js
@@ -1,4 +1,8 @@
 import React from "react";
+import imgLogo from "./pic1.jpg";
+import imgLogo2 from "./pic2.png";
+import imgApple from "./apple.png";
+import imgAndroid from "./android.png";
 
 export default function FooterBody() {
   return (
@@ -10,7 +14,49 @@ export default function FooterBody() {
         paddingBottom: "24px",
       }}
     >
-      <hr className="" />
+      <div className="container2">
+        <div
+          style={{
+            left: 0,
+            top: 0,
+            display: "flex",
+            justifyContent: "space-between",
+            padding: "5% 16% 5% 16% ",
+          }}
+        >
+          <div>
+            <h6>TIX</h6>
+            <a>FAQ</a>
+            <br />
+            <a>Brand Guidelines</a>
+            <br />
+            <a>Thỏa thuận sử dụng</a>
+            <br />
+            <a>Chính sách bảo mật</a>
+          </div>
+          <h6>Đối Tác</h6>
+          <div>
+            <h6>MOBILE APP</h6>
+            <a
+              target="_blank"
+              href="https://apps.apple.com/vn/app/tix-%C4%91%E1%BA%B7t-v%C3%A9-nhanh-nh%E1%BA%A5t/id615186197"
+              rel="noreferrer"
+            >
+              <img src={imgApple} alt="android" width="40" />
+            </a>
+            <a
+              target="_blank"
+              href="https://play.google.com/store/apps/details?id=vn.com.vng.phim123"
+              rel="noreferrer"
+            >
+              <img src={imgAndroid} alt="android" width="40" />
+            </a>
+          </div>
+          <h6>SOCIAL</h6>
+        </div>
+      </div>
+      <hr />
+
       <div
         style={{
           top: 0,
@@ -19,24 +65,43 @@ export default function FooterBody() {
           color: "#fff",
           padding: "20px 0 10px 0",
           textAlign: "left",
+          marginLeft: "30%",
+          marginBottom: "60%",
         }}
       >
-        <h6 className="font-thin text-xs">
-          TIX - SẢN PHẨM CỦA CÔNG TY CỔ PHẦN ZION{" "}
-        </h6>
-        <br />
-        <h6 className="font-thin text-xs">
-          Đia chỉ: Z06 Đường số 13, Phường Tân Thuận Đông, Quận 7, Tp. Hồ Chí
-          Minh, Việt Nam.
-        </h6>
-        <h6 className="font-thin text-xs">
-          Giấy chứng nhận đăng ký kinh doanh số: 0101659783, đăng ký thay đổi
-          lần thứ 30, ngày 22 tháng 01 năm 2020 do Sở kế hoạch và đầu tư Thành
-          phố Hồ Chí Minh cấp.
-        </h6>
-        <h6 className="font-thin text text-xs">
-          Số Điện Thoại (Hotline): 1900 545 436
-        </h6>
+        <div className="container">
+          <img
+            style={{
+              marginTop: "60px",
+              marginLeft: "20%",
+              display: "flex",
+              flexGrow: "0",
+              maxWidth: "16.666667%",
+              flexBasis: "0",
+            }}
+            src={imgLogo}
+            alt="logo"
+            width="100 "
+          />
+          <h6 className="font-thin text-xs">
+            TIX - SẢN PHẨM CỦA CÔNG TY CỔ PHẦN ZION{" "}
+          </h6>
+          <br />
+          <h6 className="font-thin text-xs">
+            Đia chỉ: Z06 Đường số 13, Phường Tân Thuận Đông, Quận 7, Tp. Hồ Chí
+            Minh, Việt Nam.
+          </h6>
+          <h6 className="font-thin text-xs">
+            Giấy chứng nhận đăng ký kinh doanh số: 0101659783, đăng ký thay đổi
+            lần thứ 30,
+            <br /> ngày 22 tháng 01 năm 2020 do Sở kế hoạch và đầu tư Thành phố
+            Hồ Chí Minh cấp.
+          </h6>
+          <h6 className="font-thin text text-xs">
+            Số Điện Thoại (Hotline): 1900 545 436
+          </h6>
+        </div>
+        <img style={{}} src={imgLogo2} alt="logo2" Width="100" />
       </div>
     </div>
   );
